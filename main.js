426388c61b8b2a60b4d57043d2eb9bc31289f82e
Luis Dutra <luisvinicius99@gmail.com>
2020-05-18 05:17:35 -0300

    refactoring the code and adding multiplayer
    add classes for player and arena

---
+++ main.js
@@ -0,0 +1,95 @@
+
+function createPiece(type) {
+  if (type === 'T') {
+   return [
+      [0, 0, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ];
+  } else if ( type === 'O') {
+    return [
+      [2, 2],
+      [2, 2],
+    ];
+  } else if ( type === 'L') {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3],
+    ];
+  } else if ( type === 'J') {
+    return [
+      [0, 4, 0],
+      [0, 4, 0],
+      [4, 4, 0],
+    ];
+  } else if ( type === 'I') {
+    return [
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+      [0, 5, 0, 0],
+    ];
+  } else if ( type === 'S') {
+    return [
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0],
+    ];
+  } else if ( type === 'Z') {
+    return [
+      [7, 7, 0],
+      [0, 7, 7],
+      [0, 0, 0],
+    ];
+  }
+}    
+
+function updateScore() {
+  document.getElementById('score').innerText = tetris.player.score;
+}
+
+const tetri = []
+
+const playerElements = document.querySelectorAll('.player');
+[...playerElements].forEach(element => {
+  const canvas = element.querySelector('canvas');
+  const tetris = new Tetris(canvas);
+  tetri.push(tetris);
+});
+
+const keyListener = (event) => {
+  [
+    //Player 1 controllers
+    //a = left, s = drop, d = right, q = rotate left, e = rotate right
+    [65, 68, 81, 69, 83],
+    //Player 2 controllers 
+    //h= left, j = drop, k = right, y = rotate left, i = rotate right
+    [72, 75, 89, 73, 74],
+  ].forEach((key, index) => {
+
+   const player = tetri[index].player;
+
+    if(event.type === 'keydown'){
+
+      if(event.keyCode === key[0]) {
+        player.move(-1);
+      } else if (event.keyCode === key[1]) {
+        player.move(1);
+      } else if (event.keyCode === key[2]) {
+        player.rotate(-1);
+      } else if (event.keyCode === key[3]) {
+        player.rotate(1);
+      }
+
+    } else if (event.keyCode === key[4]) {
+      player.drop();
+    }
+      //Tempo do video q vc parou otario 44:17
+  });
+}
+
+ document.addEventListener('keydown', keyListener);
+ document.addEventListener('keyup', keyListener);
+
+ updateScore();
